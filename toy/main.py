ce3e8ddad7e3fdee00c28affede35aa4b291c6c0
Narumi <weaper@gmail.com>
2018-01-17 12:32:13 +0800

    change size to world size

--- toy/main.py
+++ toy/main.py
@@ -8,7 +8,7 @@ def foo(rank, world_size):
     for step in range(100):
         # get random int
         value = randint(0, 10)
-        
+
         # group all ranks
         ranks = list(range(world_size))
         group = dist.new_group(ranks=ranks)
@@ -38,11 +38,11 @@ def main():
     parser.add_argument('--ip', type=str, default='127.0.0.1')
     parser.add_argument('--port', type=str, default='20000')
     parser.add_argument('--rank', '-r', type=int)
-    parser.add_argument('--size', '-s', type=int)
+    parser.add_argument('--world-size', '-s', type=int)
     args = parser.parse_args()
     print(args)
-    
-    initialize(args.rank, args.size, args.ip, args.port)
+
+    initialize(args.rank, args.world_size, args.ip, args.port)
 
 if __name__ == '__main__':
     main()
