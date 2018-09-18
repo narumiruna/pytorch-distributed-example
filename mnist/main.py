3cfcb96b58a6199de7e6371adc3b447b6200d2e6
なるみ <weaper@gmail.com>
2018-09-18 14:01:39 +0800

    Sample the dataset

--- mnist/main.py
+++ mnist/main.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn.functional as F
 from torch import distributed, nn
 from torch.utils import data
+from torch.utils.data.distributed import DistributedSampler
 from torchvision import datasets, transforms
 
 
@@ -126,10 +127,15 @@ def get_dataloader(root, batch_size):
         transforms.Normalize((0.13066047740239478,), (0.3081078087569972,))
     ])
 
+    train_set = datasets.MNIST(
+        root, train=True, transform=transform, download=True)
+    sampler = DistributedSampler(train_set)
+
     train_loader = data.DataLoader(
-        datasets.MNIST(root, train=True, transform=transform, download=True),
+        train_set,
         batch_size=batch_size,
-        shuffle=True)
+        shuffle=(sampler is None),
+        sampler=sampler)
 
     test_loader = data.DataLoader(
         datasets.MNIST(root, train=False, transform=transform, download=True),
