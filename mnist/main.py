40379ab9b7434875fe246f5564ddb17cbda4ffc4
なるみ <weaper@gmail.com>
2019-06-25 11:39:02 +0800

    Rename variables and format code

--- mnist/main.py
+++ mnist/main.py
@@ -34,9 +34,9 @@ class Accuracy(object):
         self.correct = 0
         self.count = 0
 
-    def update(self, output, label):
-        predictions = output.data.argmax(dim=1)
-        correct = predictions.eq(label.data).sum().item()
+    def update(self, output, target):
+        pred = output.data.argmax(dim=1)
+        correct = pred.eq(target.data).sum().item()
 
         self.correct += correct
         self.count += output.size(0)
@@ -51,8 +51,8 @@ class Accuracy(object):
 
 class Trainer(object):
 
-    def __init__(self, net, optimizer, train_loader, test_loader, device):
-        self.net = net
+    def __init__(self, model, optimizer, train_loader, test_loader, device):
+        self.model = model
         self.optimizer = optimizer
         self.train_loader = train_loader
         self.test_loader = test_loader
@@ -66,20 +66,21 @@ class Trainer(object):
             print(
                 'Epoch: {}/{},'.format(epoch, epochs),
                 'train loss: {}, train acc: {},'.format(train_loss, train_acc),
-                'test loss: {}, test acc: {}.'.format(test_loss, test_acc))
+                'test loss: {}, test acc: {}.'.format(test_loss, test_acc),
+            )
 
     def train(self):
+        self.model.train()
+
         train_loss = Average()
         train_acc = Accuracy()
 
-        self.net.train()
-
-        for data, label in self.train_loader:
+        for data, target in self.train_loader:
             data = data.to(self.device)
-            label = label.to(self.device)
+            target = target.to(self.device)
 
-            output = self.net(data)
-            loss = F.cross_entropy(output, label)
+            output = self.model(data)
+            loss = F.cross_entropy(output, target)
 
             self.optimizer.zero_grad()
             loss.backward()
@@ -88,33 +89,33 @@ class Trainer(object):
             self.optimizer.step()
 
             train_loss.update(loss.item(), data.size(0))
-            train_acc.update(output, label)
+            train_acc.update(output, target)
 
         return train_loss, train_acc
 
     def evaluate(self):
+        self.model.eval()
+
         test_loss = Average()
         test_acc = Accuracy()
 
-        self.net.eval()
-
         with torch.no_grad():
-            for data, label in self.test_loader:
+            for data, target in self.test_loader:
                 data = data.to(self.device)
-                label = label.to(self.device)
+                target = target.to(self.device)
 
-                output = self.net(data)
-                loss = F.cross_entropy(output, label)
+                output = self.model(data)
+                loss = F.cross_entropy(output, target)
 
                 test_loss.update(loss.item(), data.size(0))
-                test_acc.update(output, label)
+                test_acc.update(output, target)
 
         return test_loss, test_acc
 
     def average_gradients(self):
         world_size = distributed.get_world_size()
 
-        for p in self.net.parameters():
+        for p in self.model.parameters():
             t = p.grad.data.cpu()
             distributed.all_reduce(t, op=distributed.reduce_op.SUM)
             t /= float(world_size)
@@ -132,70 +133,50 @@ class Net(nn.Module):
 
 
 def get_dataloader(root, batch_size):
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.1307,), (0.3081,))])
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,)),
+    ])
 
-    train_set = datasets.MNIST(
-        root, train=True, transform=transform, download=True)
+    train_set = datasets.MNIST(root, train=True, transform=transform, download=True)
     sampler = DistributedSampler(train_set)
+    train_loader = data.DataLoader(train_set, batch_size=batch_size, shuffle=(sampler is None), sampler=sampler)
 
-    train_loader = data.DataLoader(
-        train_set,
-        batch_size=batch_size,
-        shuffle=(sampler is None),
-        sampler=sampler)
-
-    test_loader = data.DataLoader(
-        datasets.MNIST(root, train=False, transform=transform, download=True),
-        batch_size=batch_size,
-        shuffle=False)
+    test_set = datasets.MNIST(root, train=False, transform=transform, download=True)
+    test_loader = data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
     return train_loader, test_loader
 
 
 def run(args):
-    device = torch.device(
-        'cuda' if torch.cuda.is_available() and not args.no_cuda else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() and not args.no_cuda else 'cpu')
 
-    net = Net().to(device)
+    model = Net().to(device)
 
-    optimizer = torch.optim.Adam(net.parameters(), lr=args.learning_rate)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
     train_loader, test_loader = get_dataloader(args.root, args.batch_size)
 
-    trainer = Trainer(net, optimizer, train_loader, test_loader, device)
+    trainer = Trainer(model, optimizer, train_loader, test_loader, device)
     trainer.fit(args.epochs)
 
 
 def init_process(args):
     distributed.init_process_group(
-        backend=args.backend,
-        init_method=args.init_method,
-        rank=args.rank,
-        world_size=args.world_size)
+        backend=args.backend, init_method=args.init_method, rank=args.rank, world_size=args.world_size)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--backend',
-        type=str,
-        default='tcp',
-        help='Name of the backend to use.')
+    parser.add_argument('--backend', type=str, default='tcp', help='Name of the backend to use.')
     parser.add_argument(
         '-i',
         '--init-method',
         type=str,
         default='tcp://127.0.0.1:23456',
         help='URL specifying how to initialize the package.')
-    parser.add_argument(
-        '-r', '--rank', type=int, help='Rank of the current process.')
-    parser.add_argument(
-        '-s',
-        '--world-size',
-        type=int,
-        help='Number of processes participating in the job.')
+    parser.add_argument('-r', '--rank', type=int, help='Rank of the current process.')
+    parser.add_argument('-s', '--world-size', type=int, help='Number of processes participating in the job.')
     parser.add_argument('--epochs', type=int, default=20)
     parser.add_argument('--no-cuda', action='store_true')
     parser.add_argument('--learning-rate', '-lr', type=float, default=1e-3)
