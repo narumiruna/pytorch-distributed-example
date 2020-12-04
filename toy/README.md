deb8fd4768d436ef59a73c73ec408c36268ce3ce
なるみ <weaper@gmail.com>
2020-12-04 14:21:05 +0800

    Migrate toy example to pytorch v1.7.0

--- toy/README.md
+++ toy/README.md
@@ -2,10 +2,22 @@
 
 Rank 0
 ```
-$ python3 main.py --init-method tcp://127.0.0.1:23456 --rank 0 --world-size 2
+$ python3 -m torch.distributed.launch \
+    --nproc_per_node=1 \
+    --nnodes=2 \
+    --node_rank=0 \
+    --master_addr="127.0.0.1" \
+    --master_port=1234 \
+    main.py
 ```
 
 Rank 2
 ```
-$ python3 main.py --init-method tcp://127.0.0.1:23456 --rank 1 --world-size 2
+$ python3 -m torch.distributed.launch \
+    --nproc_per_node=1 \
+    --nnodes=2 \
+    --node_rank=0 \
+    --master_addr="127.0.0.1" \
+    --master_port=1234 \
+    main.py
 ```
--- toy/main.py
+++ toy/main.py
@@ -6,20 +6,26 @@ import torch
 import torch.distributed as dist
 
 
-def run(world_size, rank, steps):
+def run(local_rank, steps):
     for step in range(1, steps + 1):
         # get random int
         value = randint(0, 10)
 
         # group all ranks
-        ranks = list(range(world_size))
+        ranks = list(range(dist.get_world_size()))
         group = dist.new_group(ranks=ranks)
 
         # compute reduced sum
         tensor = torch.tensor(value, dtype=torch.int)
         dist.all_reduce(tensor, op=dist.ReduceOp.SUM, group=group)
 
-        print('rank: {}, step: {}, value: {}, reduced sum: {}.'.format(rank, step, value, tensor.item()))
+        print(
+            f'node_rank: {dist.get_rank()}'
+            f', local_rank: {local_rank}'
+            f', step: {step}'
+            f', value: {value}'
+            f', reduced sum: {tensor.item()}'
+            )
 
         sleep(1)
 
@@ -27,26 +33,14 @@ def run(world_size, rank, steps):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--backend', type=str, default='gloo', help='Name of the backend to use.')
-    parser.add_argument(
-        '-i',
-        '--init-method',
-        type=str,
-        default='tcp://127.0.0.1:23456',
-        help='URL specifying how to initialize the package.')
-    parser.add_argument('-s', '--world-size', type=int, help='Number of processes participating in the job.')
-    parser.add_argument('-r', '--rank', type=int, help='Rank of the current process.')
     parser.add_argument('--steps', type=int, default=20)
+    parser.add_argument('--local_rank', type=int, default=0)
     args = parser.parse_args()
     print(args)
 
-    dist.init_process_group(
-        backend=args.backend,
-        init_method=args.init_method,
-        world_size=args.world_size,
-        rank=args.rank,
-    )
+    dist.init_process_group(backend=args.backend)
 
-    run(args.world_size, args.rank, args.steps)
+    run(args.local_rank, args.steps)
 
 
 if __name__ == '__main__':
